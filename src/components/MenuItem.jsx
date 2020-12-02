24f750fc22972375933f249f6d78e9a6751f458d
Stefan Hristov <shristov22@gmail.com>
2020-12-01 23:03:37 -0800

    added MenuItem and description to card

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 
 const MenuItem = props => {
+  // eslint-disable-next-line react/prop-types
+  const { name, description } = props.item;
   return (
     <div>
       <Card style={{ width: '18rem' }}>
@@ -13,9 +15,9 @@ const MenuItem = props => {
           src={`https://picsum.photos/id/${Math.floor(Math.random() * 99)}/200`}
         />
         <Card.Body>
-          <Card.Title>Sample Menu Title</Card.Title>
+          <Card.Title>{name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            Sample Menu Subtitle
+            {description}
           </Card.Subtitle>
           <Card.Text>
             Some quick example text to build on the card title and make up the
