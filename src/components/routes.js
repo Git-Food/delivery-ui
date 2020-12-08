45e2ae0ea63f1b3d9ed669922b3b9f4356208d6c
Stefan Hristov <shristov22@gmail.com>
2020-12-07 17:26:17 -0800

    added orderhistory route

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,10 +1,13 @@
 import Menu from './Menu';
 import Restaurants from './Restaurants';
+import OrderHistory from './OrderHistory';
 import NotFound from './NotFound.jsx';
 
+// TODO (shh): modify orderhistory route to include userID.
 const routes = [
   { path: '/restaurants', component: Restaurants },
   { path: '/menu/:id', component: Menu },
+  { path: '/orderhistory', component: OrderHistory },
   { path: '*', component: NotFound },
 ];
 
