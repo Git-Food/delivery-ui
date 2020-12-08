60bcfbd65159a56f463b47629d6fef9ac6f49c56
Stefan Hristov <shristov22@gmail.com>
2020-12-07 17:30:28 -0800

    created Order Component

---
+++ src/components/Order.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { Card, Row, Button, Container, Col } from 'react-bootstrap';
+
+const Order = props => {
+  // console.log(props.restaurantName);
+  const {
+    orderStatus,
+    totalPrice,
+    totalOrderItemQuantity,
+    orderDate,
+  } = props.order;
+
+  // TODO (shh) create Order Status button vs Re-Order Button depending on orderStatus
+  // if OrderStatus is PENDING, UNDER_REVIEW, ACCEPTED, FOOD_BEING_PREPARED, COURIER_ON_WAY --> Order Status button
+  // if OrderStatus is DELIVERED, REJECTED_BY_RESTAURANT, NO_COURIER_AVAILABLE, REFUNDED, CANCELLED --> Re-Order button
+
+  // function handleFindRestaurant() {
+  //   props.findRestaurantName(props.order.businessId);
+  // }
+
+  // const restaurantName = props.findRestaurantName(props.order.businessId);
+
+  return (
+    <Container>
+      <Card className="mb-3">
+        <Row>
+          <Col className="center">
+            <Card.Img
+              src={`https://picsum.photos/id/${Math.floor(
+                Math.random() * 99
+              )}/200`}
+              style={{ maxWidth: '200px', minWidth: '200px' }}
+            />
+          </Col>
+          <Col className="col-md-8 col-xs-12">
+            <Card.Body className="text-md-right text-xs-left">
+              <Card.Title className="card-title">
+                Fetch Restaurant Name
+              </Card.Title>
+              <Card.Text>
+                {orderDate}
+                <br />
+                {totalOrderItemQuantity} items
+                <br />${totalPrice.toFixed(2)}
+                <br />
+                {orderStatus}
+                <br />
+                <Button>Re-Order</Button>
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+  );
+};
+
+Order.propTypes = {
+  order: PropTypes.object,
+};
+
+export default Order;
