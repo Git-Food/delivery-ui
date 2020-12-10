1532c41081a0b3091cb7c16421e20062d3d6da76
Stefan Hristov <shristov22@gmail.com>
2020-12-10 15:37:39 -0800

    refactor Orders to use Redux for Restaurants, and render alternate view for no orderhistory

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -3,23 +3,15 @@ import PropTypes from 'prop-types';
 
 import { loadOrders } from '../store/orders';
 import { connect } from 'react-redux';
-
+import { Jumbotron, Container } from 'react-bootstrap';
+import { loadRestaurants } from '../store/restaurants';
 import Order from './Order';
 
 class Orders extends Component {
   componentDidMount() {
     this.props.loadOrders();
+    this.props.loadRestaurants();
   }
-
-  // TO DO (shh): need to fetch restaurant name for every order
-  // async fetchRestaurant(businessId) {
-  //   const data = fetch(
-  //     `https://git-food.herokuapp.com/restaurant/5fca9eb5e4e763507dc3aab3`
-  //   );
-  //   const restaurant = data.json();
-  //   console.log(restaurant.name);
-  //   return restaurant.name;
-  // }
 
   // TO DO (shh): need to create fetcher for orders only by specific userID
 
@@ -27,13 +19,22 @@ class Orders extends Component {
     return (
       <>
         <h1>Placeholder for OrderHistory View</h1>
-        {this.props.orders.map(order => (
-          <Order
-            key={order.id}
-            order={order}
-            // findRestaurantName={this.fetchRestaurant}
-          />
-        ))}
+        {this.props.orders ? (
+          this.props.orders.map(order => (
+            <Order
+              key={order.id}
+              order={order}
+              restaurants={this.props.restaurants}
+            />
+          ))
+        ) : (
+          <Jumbotron fluid>
+            <Container className="text-center">
+              <h2>No Past Orders</h2>
+              <p>Are you never hungry?</p>
+            </Container>
+          </Jumbotron>
+        )}
       </>
     );
   }
@@ -41,15 +42,19 @@ class Orders extends Component {
 
 const mapStateToProps = state => ({
   orders: state.entities.orders.list,
+  restaurants: state.entities.restaurants.list,
 });
 
 const mapDispatchToProps = dispatch => ({
   loadOrders: () => dispatch(loadOrders()),
+  loadRestaurants: () => dispatch(loadRestaurants()),
 });
 
 Orders.propTypes = {
   orders: PropTypes.array,
   loadOrders: PropTypes.func,
+  restaurants: PropTypes.array,
+  loadRestaurants: PropTypes.func,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Orders);
