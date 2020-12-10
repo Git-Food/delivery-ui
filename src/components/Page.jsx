cb4fd9a693698253dfbed6e7b5f23b272a3ae941
Stefan Hristov <shristov22@gmail.com>
2020-12-09 20:24:03 -0800

    removed unused imports from Page.jsx

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
-  Button,
-  Container,
-} from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import ShoppingCart from './ShoppingCart.jsx';
 
