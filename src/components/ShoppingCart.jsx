6ac40f7cb75186d980b178aabb336b2bb381f70a
Stefan Hristov <shristov22@gmail.com>
2020-12-15 22:35:02 -0800

    added user context, empty shopping cart jumbotron

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import {
   loadShoppingCart,
@@ -8,7 +9,7 @@ import {
   decrementOrderItem,
 } from '../store/shoppingCart';
 import OrderItem from './OrderItem';
-import { useAuth } from '../store/AuthContext';
+import AuthContext from '../store/AuthContext';
 
 import {
   NavItem,
@@ -18,12 +19,15 @@ import {
   Tooltip,
   OverlayTrigger,
   Row,
+  Jumbotron,
+  Container,
 } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 class ShoppingCart extends Component {
+  static contextType = AuthContext;
   constructor(props) {
     super(props);
     this.state = {
@@ -34,7 +38,10 @@ class ShoppingCart extends Component {
   }
 
   componentDidMount() {
-    this.props.loadShoppingCart();
+    const user = this.context.currentUser;
+    if (user) {
+      this.props.loadShoppingCart(user.uid);
+    }
   }
 
   showModal() {
@@ -53,7 +60,7 @@ class ShoppingCart extends Component {
 
   render() {
     const { showing } = this.state;
-    const { currentUser } = useAuth;
+    const user = this.context.currentUser;
     return (
       <React.Fragment>
         <NavItem onClick={this.showModal}>
@@ -69,32 +76,42 @@ class ShoppingCart extends Component {
             <Modal.Title>Shopping Cart</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            {Object.entries(this.props.shoppingCart.items).map(([k, value]) => (
-              <Row key={k + '1'}>
-                {/* Pass props to child component, userid is harcoded for now */}
-                {/* TODO: (pcg) replace harcoded user id */}
-                <OrderItem
-                  key={k}
-                  orderItem={value}
-                  onIncrement={this.props.incrementOrderItem}
-                  onDecrement={this.props.decrementOrderItem}
-                  // userid={currentUser.id}
-                />
-              </Row>
-            ))}
+            {this.props.shoppingCart.quantity ? (
+              Object.entries(this.props.shoppingCart.items).map(
+                ([k, value]) => (
+                  <Row key={k + '1'}>
+                    <OrderItem
+                      key={k}
+                      orderItem={value}
+                      onIncrement={this.props.incrementOrderItem}
+                      onDecrement={this.props.decrementOrderItem}
+                      userid={user.uid}
+                    />
+                  </Row>
+                )
+              )
+            ) : (
+              <Jumbotron fluid>
+                <Container className="text-center">
+                  <h2>No Items</h2>
+                  <p>What are you waiting for? Get some food in your belly!</p>
+                </Container>
+              </Jumbotron>
+            )}
           </Modal.Body>
           <Modal.Footer>
             <Row key="">
               Total Price: ${this.props.shoppingCart.price.toFixed(2)}
             </Row>
             <ButtonToolbar>
-              <Button
-                variant="outline-dark"
-                onClick={this.hideModal}
-                disabled={this.props.shoppingCart.empty}
-                href="/checkout">
-                Checkout
-              </Button>
+              <Link to="/checkout">
+                <Button
+                  variant="outline-dark"
+                  onClick={this.hideModal}
+                  disabled={this.props.shoppingCart.empty}>
+                  Checkout
+                </Button>
+              </Link>
             </ButtonToolbar>
           </Modal.Footer>
         </Modal>
