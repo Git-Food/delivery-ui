58d04631bdaeae3b93adf170a6f572b63c968f9b
Pedro Cantu <35717149+pedrocantu16@users.noreply.github.com>
2020-12-16 21:34:43 -0800

    updated function

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Table, Form, FormGroup, Button, Col, Card } from 'react-bootstrap';
 import { loadShoppingCart, checkout } from '../store/shoppingCart';
 import { useAuth } from '../store/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
@@ -14,14 +15,16 @@ export default function Checkout() {
   const dispatch = useDispatch();
   const shoppingCart = useSelector(state => state.entities.shoppingCart);
   const { currentUser } = useAuth();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(loadShoppingCart(currentUser.uid));
   }, []);
 
-  const placeOrder = () => {
-    dispatch(checkout(currentUser.uid));
-  };
+  async function placeOrder() {
+    await dispatch(checkout(currentUser.uid));
+    history.push('/orders');
+  }
 
   return (
     <>
@@ -103,8 +106,7 @@ export default function Checkout() {
                 variant="success"
                 disabled={shoppingCart.empty}
                 // TODO (pcg): replace user id
-                onClick={placeOrder}
-                href="/orders">
+                onClick={placeOrder}>
                 Place Order
               </Button>
             </td>
